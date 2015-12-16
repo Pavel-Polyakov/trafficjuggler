5721f1adc91f83b8379305820bba2bb86bb581eb
laneus <polyakov.pavel.xyz@gmail.com>
2015-12-16 15:57:31 +0300

    new views

--- mpls.py
+++ mpls.py
@@ -390,9 +390,30 @@ if __name__ == "__main__":
     router = mRouter(host,zapi)
     router.parse()
 
+    print "\nSummary information"
     for interface in router.intlist.sortByOutput():
         interface.printInterface()
         router.lsplist.getLSPByInterface(interface.name).printLSPList()
         print
+
+    print "\nInformation related to Hosts"
+    for host in router.lsplist.getAllHostsSortedByOutput():
+        lsps = router.lsplist.getLSPByHost(host)
+        print '{host:<15}{space:<21}{bandwidth:<12}{output:<12}{rband:<12}'.format(space = ' ',
+                                                                                    host=host,
+                                                                                    output=str(round(float(lsps.getSumOutput())/1000,1))+"Gbps", 
+                                                                                    bandwidth=str(lsps.getSumBandwidth())+"m", 
+                                                                                    rband=str(lsps.getAverageRBandwidthByHost(host))+"m")
+    
+    print "\nInformation related to Interface"
+    for interface in router.intlist.sortByOutput():
+        speed = re.sub('Gbps','000',interface.speed)
+        output = interface.output                                                                                                                                                                                                                  
+        utilization = str(round(output/float(speed)*100,2))+"%"
+        print '{name:<15}{description:<21}{speed:<12}{output:<12}{util:<15}'.format(name = interface.name, 
+                                                                                    description = interface.description, 
+                                                                                    speed = interface.speed, 
+                                                                                    output = str(round(float(interface.output)/1000,1))+"Gbps", 
+                                                                                    util = utilization)
 
     router.close()
