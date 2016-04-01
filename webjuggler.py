07191bfb86b91c2e97eb393721a5bb6aa2f92113
laneus <polyakov.pavel.xyz@gmail.com>
2016-04-01 03:17:07 +0300

    fixed bug with wrong using of model; cleaning code

--- webjuggler.py
+++ webjuggler.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from datetime import datetime, timedelta
+from copy import copy
+import logging
+import sys
+
+from config import FULL_PATH, HOSTS
 from TrafficJuggler.models.lsp import LSP
 from TrafficJuggler.models.interface import Interface
 from TrafficJuggler.models.host import Host
 from TrafficJuggler.models.image import Image
-from TrafficJuggler.parser import Parser
 from plotter import getGraph
-from sqlalchemy.sql import func
-from sqlalchemy import or_
-import logging
-import sys
-from datetime import datetime, timedelta
-from config import FULL_PATH, HOSTS
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{path}/tj.db'.\
@@ -39,10 +40,11 @@ def index():
         routers.append(r)
 
     return render_template('index.html',
-                            routers=routers)
+                           routers=routers)
+
 
 @app.route('/<router>/lsp/<key>.png')
-def plot_lsp(router,key):
+def plot_lsp(router, key):
     xy = session.query(LSP.output, Image.time).\
             filter(Image.router == router).\
             filter(LSP.name == key).\
@@ -50,13 +52,14 @@ def plot_lsp(router,key):
             filter(Image.time > datetime.now() - timedelta(days=1, hours=3)).\
             all()
     x = [k[1] for k in xy]
-    x = map(setMowTime,x)
+    x = map(setMowTime, x)
     y = [k[0] for k in xy]
-    y = [0 if k == None else k for k in y]
-    return getGraph(x,y)
+    y = [0 if k is None else k for k in y]
+    return getGraph(x, y)
+
 
 @app.route('/<router>/interface/<key>.png')
-def plot_interface(router,key):
+def plot_interface(router, key):
     xy = session.query(Interface.output, Image.time).\
             filter(Image.router == router).\
             filter(Interface.description == key).\
@@ -64,103 +67,122 @@ def plot_interface(router,key):
             filter(Image.time > datetime.now() - timedelta(days=1, hours=3)).\
             all()
     x = [k[1] for k in xy]
-    x = map(setMowTime,x)
+    x = map(setMowTime, x)
     y = [k[0] for k in xy]
-    y = [0 if k == None else k for k in y]
-    return getGraph(x,y)
+    y = [0 if k is None else k for k in y]
+    return getGraph(x, y)
+
 
 @app.route('/<router>/host/<key>.png')
-def plot_host(router,key):
+def plot_host(router, key):
     images = session.query(Image).\
-            filter(Image.router == router).\
-            filter(Image.time > datetime.now() - timedelta(days=1, hours=3)).all()
+        filter(Image.router == router).\
+        filter(Image.time > datetime.now() - timedelta(days=1, hours=3)).all()
     x = [k.time for k in images]
-    x = map(setMowTime,x)
+    x = map(setMowTime, x)
     HostOutput = []
     for image in images:
         output = session.query(func.sum(LSP.output)).\
                 filter(LSP.image_id == image.id).\
                 filter(LSP.to == key).scalar()
-        if output == None: output = 0
+        if output is None:
+            output = 0
         HostOutput.append(output)
     y = HostOutput
-    return getGraph(x,y)
+    return getGraph(x, y)
 
-@app.route('/<router>/<key>s')
-def plot_list(router,key):
+
+@app.route('/<router>/<key>')
+def plot_list(router, key):
     last_parse = session.query(Image).filter(Image.router == router).all()[-1]
     last_parse_id = last_parse.id
-    last_parse_time = setMowTime(last_parse.time)
-    if key == 'interface':
+
+    if key == 'interfaces':
         f = getInterfacesByImageId
         val_compared = 'description'
         val_out = 'output'
-    elif key == 'host':
+    elif key == 'hosts':
         f = getHostsByImageId
         val_compared = 'ip'
         val_out = 'sumoutput'
-    elif key == 'lsp':
+    elif key == 'lsps':
         f = getLSPSByImageId
         val_compared = 'name'
         val_out = 'output'
+
     elements = f(last_parse_id)
     for element in elements:
-        element.img = '/{router}/{key}/{val}.png'.format(router = router, key = key, val = getattr(element, val_compared))
+        element.img = '/{router}/{key}/{val}.png'.\
+            format(router=router,
+                   key=re.sub('s$', '', key),
+                   val=getattr(element, val_compared))
         element.comment = getattr(element, val_compared)
-        element.out = getattr(element,val_out)
+        element.out = getattr(element, val_out)
     return render_template('list.html',
-                            elements=elements)
+                           elements=elements)
+
 
 def getHostsByImageId(id):
+    result = []
     hosts = session.query(Host).\
-                            filter(Host.ip == LSP.to).\
-                            filter(LSP.image_id == id).\
-                            distinct(LSP.to).all()
+        filter(Host.ip == LSP.to).\
+        filter(LSP.image_id == id).\
+        distinct(LSP.to).all()
     for host in hosts:
+        H = copy(host)
         query = session.query(LSP).\
-                                filter(LSP.image_id == id).\
-                                filter(LSP.to == host.ip)
+            filter(LSP.image_id == id).\
+            filter(LSP.to == host.ip)
         lsplist = query.cte()
-        host.lsplist = session.query(lsplist, Host.name.label('to_name')).\
-                                filter(lsplist.c.to == Host.ip).\
-                                order_by(lsplist.c.output.desc()).all()
-        subq_b = session.query(func.sum(lsplist.c.bandwidth).label('sumbandwidth')).subquery()
-        subq_o = session.query(func.sum(lsplist.c.output).label('sumoutput')).subquery()
-        subq_r = session.query(func.avg(lsplist.c.rbandwidth).label('rbandwidth')).subquery()
-        host.sumbandwidth,host.sumoutput,host.rbandwidth = \
-                                session.query(subq_b.c.sumbandwidth,
-                                subq_o.c.sumoutput,
-                                subq_r.c.rbandwidth).first()
+        H.lsplist = session.query(lsplist, Host.name.label('to_name')).\
+            filter(lsplist.c.to == Host.ip).\
+            order_by(lsplist.c.output.desc()).all()
+        subq_b = session.query(func.sum(lsplist.c.bandwidth).
+                               label('sumbandwidth')).subquery()
+        subq_o = session.query(func.sum(lsplist.c.output).
+                               label('sumoutput')).subquery()
+        subq_r = session.query(func.avg(lsplist.c.rbandwidth).
+                               label('rbandwidth')).subquery()
+        H.sumbandwidth, H.sumoutput, H.rbandwidth = \
+            session.query(subq_b.c.sumbandwidth,
+                          subq_o.c.sumoutput,
+                          subq_r.c.rbandwidth).first()
+        result.append(H)
 
-    hosts = sorted(hosts, key = lambda x: x.sumoutput, reverse = True)
-    return hosts
+    result_sorted = sorted(result, key=lambda x: x.sumoutput, reverse=True)
+    return result_sorted
+
 
 def getInterfacesByImageId(id):
+    result = []
     interfaces = session.query(Interface).\
-                    filter(Interface.image_id == id).all()
+        filter(Interface.image_id == id).all()
     for interface in interfaces:
+        I = copy(interface)
         query = session.query(LSP).\
-                        filter(LSP.image_id == id).\
-                        filter(LSP.interface_id == interface.id)
+            filter(LSP.image_id == id).\
+            filter(LSP.interface_id == interface.id)
         lsplist = query.cte()
-        interface.lsplist = session.query(lsplist, Host.name.label('to_name')).\
-                        filter(lsplist.c.to == Host.ip).\
-                        order_by(lsplist.c.output.desc()).all()
-        interface.rsvpout = session.query(func.sum(lsplist.c.output)).scalar()
-        if interface.output and interface.rsvpout:
-            interface.ldpout = interface.output - interface.rsvpout
+        I.lsplist = session.query(lsplist, Host.name.label('to_name')).\
+            filter(lsplist.c.to == Host.ip).\
+            order_by(lsplist.c.output.desc()).all()
+        I.rsvpout = session.query(func.sum(lsplist.c.output)).scalar()
+        if I.output and I.rsvpout:
+            I.ldpout = I.output - I.rsvpout
         else:
-            interface.ldpout = interface.output
-        if interface.ldpout < 100:
-            interface.ldpout = 0
-    interfaces = sorted(interfaces, key = lambda x: x.output, reverse = True)
-    return interfaces
+            I.ldpout = I.output
+        if I.ldpout < 100:
+            I.ldpout = 0
+        result.append(I)
+
+    result_sorted = sorted(result, key=lambda x: x.output, reverse=True)
+    return result_sorted
+
 
 def getLSPSByImageId(id):
     lsps = session.query(LSP).\
                     filter(LSP.image_id == id).all()
-
-    lsps = sorted(lsps, key = lambda x: x.output, reverse = True)
+    lsps = sorted(lsps, key=lambda x: x.output, reverse=True)
     return lsps
 
 
@@ -170,7 +192,6 @@ def setMowTime(x):
 if __name__ == '__main__':
 
     app.run(
-#        host = "127.0.0.1",
-        host = "0.0.0.0",
-        debug = True
+        host="0.0.0.0",
+        debug=True
     )
